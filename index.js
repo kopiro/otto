fcd00369e5e790e5ed802d214e9a71d359eb925f
Flavio De Stefano <flavio.destefano@caffeina.com>
2017-02-15 21:27:25 +0100

    Using Wit.AI

--- index.js
+++ index.js
@@ -1,20 +1,109 @@
-var AI = require('./ai');
 var IO = require('./io');
 
-var myAI = new AI();
+[
+[ 'warn',  '\x1b[35m' ],
+[ 'error', '\x1b[31m' ],
+[ 'info',   '\x1b[2m' ],
+[ 'debug',   '\x1b[30m' ],
+[ 'user',   '\x1b[35m' ],
+[ 'ai',   '\x1b[35m' ],
+].forEach(function(pair) {
+	var method = pair[0], reset = '\x1b[0m', color = '\x1b[36m' + pair[1];
+	console[method] = (console[method] || console.log).bind(console, color, '[' + method.toUpperCase() + ']', reset);
+});
 
-function conversate() {
+const config = require('./config.json');
+const {Wit, log, interactive} = require('node-wit');
+
+const WitClient = new Wit({
+	accessToken: config.WIT_AI_TOKEN,
+	actions: {
+
+		send(request, response) {
+			console.info('AI.send', request, response);
+			return IO.output(response.text);
+		},
+
+		sayHello(request) {
+			console.info('AI.sayHello', request);
+			return new Promise(function(resolve, reject) {
+				resolve({
+					user: request.entities.contact[0].value
+				});
+			});
+		},
+
+		tellNameOf(request) {
+			console.info('AI.tellNameOf', request);
+			return new Promise(function(resolve, reject) {
+				var who = request.entities.contact[0].value.toLowerCase();
+				switch (who) {
+					case 'mamma':
+					resolve({ phrase: 'La mia mamma è Valentina' });
+					break;
+					case 'papà':
+					resolve({ phrase: 'Il mio papà è Flavio!' });
+					break;
+					default:
+					reject({ 
+						text: 'Non so chi sia ' + who
+					});
+					break;					
+				}
+			});
+		},
+
+		setAlarm(request) {
+			console.info('AI.setAlarm', JSON.stringify(request));
+			return new Promise(function(resolve, reject) {
+				var moment = require('moment');
+				moment.locale('it');
+
+				var when = moment(request.entities.datetime[0].value);
+				if (when.diff(moment()) < 0) {
+					reject({
+						text: "Non posso ancora andare indietro nel tempo"
+					});
+					return;
+				}
+
+				var when_human = when.calendar();
+
+				resolve({
+					alarmTime: when_human
+				});
+			});
+		}
+
+	},
+	// logger: new log.Logger(log.DEBUG)
+});
+
+let sessionId = Date.now();
+let context = {};
+
+function converse() {
+	console.info('Starting conversation');
+
 	IO.input()
-	.then(myAI.startConversation.bind(myAI))
-	.then(IO.output)
-	.catch(function(err) {
-		console.error('CONVERSATION_ERROR', err);
+
+	.then(function(data) {
+		return WitClient.runActions(sessionId, data.text, context);
 	})
-	.then(function() {
-		setTimeout(conversate, 1000);
-	});
+
+	.catch(function(err) {
+		context = {};
+		sessionId = Date.now();
+		console.error('Resetting conversation');
+
+		if (err.text) {
+			return IO.output(err.text);
+		}
+	})
+
+	// Finally
+	.then(converse);
 }
 
-myAI.init().then(
-	conversate
-);
+converse();
+// interactive(WitClient);
--- io.js
+++ io.js
@@ -6,13 +6,8 @@ const Speech = require('@google-cloud/speech')({
 	keyFilename: 'gcloud.json'
 });
 
-function inViaCode() {
-	return new Promise(function(resolve, reject) {
-		resolve({
-			text: 'che tempo fa a Roma'
-		});
-	});
-}
+var AI_NAME_REGEX = /.*(otto|8|8:00) /;
+AI_NAME_REGEX = /^/;
 
 function inViaSpeech() {
 	var self = this;
@@ -33,7 +28,6 @@ function inViaSpeech() {
 		});
 
 		speechRecognizer.on('data', function(data) {
-			console.log('IO.inViaSpeech:', data.endpointerType);
 			switch (data.endpointerType) {
 
 				case 'START_OF_SPEECH':
@@ -41,17 +35,25 @@ function inViaSpeech() {
 
 				case 'ENDPOINTER_EVENT_UNSPECIFIED':
 				var phrase = data.results;
-				console.log('IO: Phrase recognized:', phrase);
-				recognized = true;
-				resolve({
-					text: phrase
-				});
+				console.info('IO.speechData', 'Recognized: ' + phrase);
+
+				if (AI_NAME_REGEX.test(phrase)) {
+					console.info('IO.speechData', 'Keyword activation!');
+					recognized = true;
+					phrase = phrase.replace(AI_NAME_REGEX, '');
+
+					console.user(phrase);
+
+					resolve({
+						text: phrase
+					});
+				}
 
 				case 'END_OF_UTTERANCE':
 				case 'END_OF_AUDIO':
 				if (processing) {
 					processing = false;
-					console.log('IO: Stopped listening');
+					console.info('IO.speechData', 'Stopped listening');
 					Recorder.stop();
 					self.timeout = setTimeout(function() {
 						if (!recognized) {
@@ -71,11 +73,15 @@ function inViaSpeech() {
 		});
 		//recordingSteam.pipe(fs.createWriteStream('/tmp/test.wav'));
 		recordingSteam.pipe(speechRecognizer);
+
+		console.info('IO.startListening');
+
 	});
 }
 
 function outViaSpeech(text) {
 	return new Promise(function(resolve, reject) {
+		console.ai(text);
 		var childD = child_process.spawn('./out-speech.sh', [ text ]);
 		childD.addListener('exit', function (code, signal) {
 			childD = null;
@@ -84,12 +90,34 @@ function outViaSpeech(text) {
 	});
 }
 
+
+function inViaCode() {
+	inViaCode.$ = inViaCode.$ || require('fs').readFileSync('in.txt').toString().split("\n");
+	return new Promise(function(resolve, reject) {
+		if (inViaCode.$.length == 0) {
+			process.exit();
+		}
+
+		var msg = inViaCode.$.shift();
+		console.user(msg);
+
+		resolve({
+			text: msg
+		});
+	});
+}
+
 function outViaStd(text) {
-	// console.log(text);
+	return new Promise(function(resolve, reject) {
+		console.ai(text);
+		resolve();
+	});
 }
 
 exports.input = inViaSpeech;
 exports.output = outViaSpeech;
 
-// exports.input = inViaCode;
-// exports.output = outViaStd;
+if (0) {
+	exports.input = inViaCode;
+	exports.output = outViaStd;
+}
