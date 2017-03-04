2cd60577ccd71f1ff0002a566e99ce8ead84ba50
Flavio De Stefano <destefano.flavio@gmail.com>
2017-03-04 16:13:58 +0100

    Added start listen

--- bin/start.sh
+++ bin/start.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-if [ "$(uname)" == "Darwin" ]; then
-	"$DIR/../out-speech.sh" "Buongiorno, sono pronto!"
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-	"$DIR/../out-speech.sh" "Buongiorno, sono pronto!"
-fi
+play "$DIR/../audio/startlisten.wav"
