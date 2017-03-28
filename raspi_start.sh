1dc13cfa5cf1bf95adacae7d8ae251005474bbd0
Flavio De Stefano <destefano.flavio@gmail.com>
2017-03-28 17:41:30 +0200

    Dont touch bluez

--- raspi_start.sh
+++ raspi_start.sh
@@ -8,14 +8,15 @@ BLUE_ID="FC:58:FA:35:48:95"
 BLUEZ_CARD="FC_58_FA_35_48_95"
 
 echo "Starting PulseAudio..."
+pulseaudio --k
 pulseaudio --start
 
 while true; do
 
-	echo "Powering on bluetooth..."
-	echo -e "power on" | bluetoothctl
-	echo -e "trust $BLUE_ID" | bluetoothctl
-	echo -e "pair $BLUE_ID" | bluetoothctl
+	# echo "Powering on bluetooth..."
+	# echo -e "power on" | bluetoothctl
+	# echo -e "trust $BLUE_ID" | bluetoothctl
+	# echo -e "pair $BLUE_ID" | bluetoothctl
 
 	while [[ "$(pacmd list-sinks | grep bluez_card.$BLUEZ_CARD)" == "" ]]; do
 		echo "Connecting to bluetooth speaker..."
