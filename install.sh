5380125d08fb9871020552933aee414d7a725477
progress44 <sinanajani@outlook.com>
2017-04-23 12:45:41 +0200

    Added safety check to install.sh. Wraped the code in curly brackets so it fails if the download is interrupted.

--- install.sh
+++ install.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
-if [ "$(uname)" == "Darwin" ]; then
+{
+	if [ "$(uname)" == "Darwin" ]; then
 
-	brew install sox
+		brew install sox
 
-	brew tap mopidy/mopidy
-	brew install mopidy
-	brew install mopidy-spotify
+		brew tap mopidy/mopidy
+		brew install mopidy
+		brew install mopidy-spotify
 
-	brew install libav
+		brew install libav
 
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+	elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 
-	wget -O - https://raw.githubusercontent.com/sdesalas/node-pi-zero/master/install-node-v7.7.1.sh | bash
-	npm -g install yarn
-	ln -svf /opt/nodejs/yarn /usr/bin/yarn
+		wget -O - https://raw.githubusercontent.com/sdesalas/node-pi-zero/master/install-node-v7.7.1.sh | bash
+		npm -g install yarn
+		ln -svf /opt/nodejs/yarn /usr/bin/yarn
 
-	apt-get -y install libsox-fmt-mp3
-	
-	apt-get -y install sox
-	
-	apt-get -y install mopidy
-	apt-get -y install mopidy-spotify
+		apt-get -y install libsox-fmt-mp3
+		
+		apt-get -y install sox
+		
+		apt-get -y install mopidy
+		apt-get -y install mopidy-spotify
 
-	apt-get -y install libav-tools
+		apt-get -y install libav-tools
 
-else
+	else
 
-	echo "Platform not supported"
-	exit
+		echo "Platform not supported"
+		exit
 
-fi
+	fi
 
-yarn
+	yarn
+}
