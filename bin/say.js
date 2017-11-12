2cddcc50eea28e67fbe45a639f244ed995f1f7ad
Flavio De Stefano <destefano.flavio@gmail.com>
2017-11-12 17:11:10 +0100

    Say command

--- bin/say.js
+++ bin/say.js
@@ -1,10 +1,24 @@
 #!/usr/bin/env node
+
 require('../boot');
 
 const TAG = 'Say';
+
 const Polly = apprequire('polly');
+const Play = apprequire('play');
 
 if (process.argv[2] == null) {
 	console.log('Usage: ./say.js "[TEXT]"');
 	process.exit(1);
 }
+
+let text = process.argv[2]; 
+async.eachSeries(Util.mimicHumanMessage(text), (t, next) => { 
+	Polly.getAudioFile(t).then((polly_file) => { 
+		Play.fileToSpeaker(polly_file, (err) => { if 
+			(err) return console.error(err); next();
+		});
+	});
+}, () => {
+	process.exit(0);
+});
