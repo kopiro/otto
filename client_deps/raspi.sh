7d6e94affd23f6b6c801cae01eff498f3161424f
Flavio De Stefano <destefano.flavio@gmail.com>
2017-11-13 23:27:35 +0100

    FIx paths

--- client_deps/raspi.sh
+++ client_deps/raspi.sh
@@ -2,7 +2,6 @@
 
 # Install nodeJS
 wget -O - https://raw.githubusercontent.com/sdesalas/node-pi-zero/master/install-node-v7.7.1.sh | bash
-ln -svf /opt/nodejs/yarn /usr/bin/yarn
 
 # Install base libraries
 apt-get -y install sox libsox-fmt-mp3 opus-tools 
@@ -15,4 +14,4 @@ npm install --only=prod
 
 # Install the compiled grpc
 mkdir -p ./node_modules/grpc/src/node/extension_binary/node-v57-linux-arm/
-ln -svf /root/grpc/build/Release/grpc_node.node ./node_modules/grpc/src/node/extension_binary/node-v57-linux-arm/grpc_node.node
+ln -svf /root/grpc_node.node ./node_modules/grpc/src/node/extension_binary/node-v57-linux-arm/grpc_node.node
