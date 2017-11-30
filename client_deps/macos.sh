415beadd5b4a3f06a3c5ebeac3f4111a62c41897
Flavio De Stefano <destefano.flavio@gmail.com>
2017-11-30 22:34:08 +0100

    Fix deps

--- client_deps/macos.sh
+++ client_deps/macos.sh
@@ -7,5 +7,5 @@ brew install opus-tools
 brew install mopidy
 
 npm -g install nan && npm -g install snowboy && npm link snowboy
-npm -g install miio && npm -g install miio
+npm -g install miio && npm link miio
 npm install
