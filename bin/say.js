6520f6434bdcbd6f9c5e589bb0e4c576626a2a1f
Flavio De Stefano <destefano.flavio@gmail.com>
2017-12-09 23:22:47 +0100

    Added repeat and ssml in polly

--- bin/say.js
+++ bin/say.js
@@ -9,20 +9,20 @@ const async = require('async');
 const Polly = apprequire('polly');
 const Play = apprequire('play');
 
-function sendMessage(text, language) {
-	return new Promise((resolve, reject) => {
-		language = language || sessionModel.getTranslateTo();
-		
-		const sentences = mimicHumanMessage(text);
-		sentences.forEach(async(sentence) => {
+async function sendMessage(text, language = 'it') {
+	const key = md5(text);
+	currentSendMessageKey = key;
+	
+	const sentences = mimicHumanMessage(text);
+
+	for (let sentence of sentences) {
+		if (currentSendMessageKey === key) {
 			let polly_file = await Polly.getAudioFile(sentence, { language: language });
 			await Play.fileToSpeaker(polly_file);
-		});
+		}
+	}
 
-		emitter.emit('ai-spoken');
-
-		resolve();
-	});
+	return true;
 }
 
 if (process.argv[2] == null) {
--- src/actions/repeat/index.js
+++ src/actions/repeat/index.js
@@ -1,11 +1,25 @@
 exports.id = 'repeat';
 
-module.exports = function({ sessionId, result }) {
-	return new Promise((resolve, reject) => {
+const _ = require('underscore');
+
+module.exports = function({ sessionId, result }, session_model) {
+	return new Promise(async(resolve, reject) => {
 		let { parameters: p, fulfillment } = result;
 
-		return resolve({
-			speech: p.q
-		});
+		if (_.isEmpty(p.driver)) {
+			return resolve({
+				speech: p.q
+			});
+		}
+
+		let session_model_by_driver = await session_model.getRelatedSessions().findOne({ io_id: p.driver });
+		if (session_model_by_driver == null) {
+			return reject();
+		}
+
+		IOManager.output({
+			speech: p.q,
+		}, session_model_by_driver);
+		return resolve({});
 	});
 };
