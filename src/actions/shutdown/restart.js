f8d7b67f2eebf4d693de46e7b455632608ac966b
Flavio De Stefano <destefano.flavio@gmail.com>
2017-12-09 22:56:31 +0100

    Added shutdown actions

---
+++ src/actions/shutdown/restart.js
@@ -0,0 +1,20 @@
+exports.id = 'shutdown.restart';
+
+const exec = require('child_process').exec;
+
+module.exports = function({ sessionId, result }, session_model) {
+	return new Promise(async(resolve, reject) => {
+		let { parameters: p, fulfillment } = result;
+
+		exec('shutdown -r now', (err, stdout, stderr) => {
+			if (err) return reject(stderr);
+			resolve({
+				speech: fulfillment.speech,
+				data: {
+					stdout: stdout,
+					feedback: true
+				}
+			});
+		});
+	});
+};
