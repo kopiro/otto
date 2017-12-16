4b104b4f3a2ba3dd6ff36154f9289f407f8b3e8d
Flavio De Stefano <destefano.flavio@gmail.com>
2017-12-16 15:34:53 +0100

    Fix AWH

--- src/awh.js
+++ src/awh.js
@@ -19,7 +19,7 @@ Router.post('/', async(req, res) => {
 	const sessionId = body.sessionId;
 
 	// From AWH can came any session ID, so ensure it exists on our DB
-	let session = IOManager.getSession(sessionId);
+	let session = await IOManager.getSession(sessionId);
 	if (session == null) {
 		console.error(TAG, `creating a missing session ID with ${sessionId}`);
 		session = new Data.Session({ _id: sessionId });
