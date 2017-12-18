8eeb83290bc52f097bd4981af42a1d55e3305176
Flavio De Stefano <destefano.flavio@gmail.com>
2017-12-18 21:46:24 +0100

    Added events

--- src/io_accessories/leds.js
+++ src/io_accessories/leds.js
@@ -12,30 +12,37 @@ exports.attach = function(io) {
 	RaspiLeds.off();
 
 	io.emitter.on('input', () => {
+		console.debug(TAG, 'input');
 		RaspiLeds.animateRandom();
 	});
 
 	io.emitter.on('output', () => {
+		console.debug(TAG, 'output');
 		RaspiLeds.setColor(colorForOutput);
 	});
 
 	io.emitter.on('thinking', () => {
+		console.debug(TAG, 'thinking');
 		RaspiLeds.animateRandom();
 	});
 
 	io.emitter.on('recognizing', () => {
+		console.debug(TAG, 'recognizing');
 		RaspiLeds.setColor(colorForRecognizing);
 	});
 
 	io.emitter.on('notrecognizing', () => {
+		console.debug(TAG, 'notrecognizing');
 		RaspiLeds.off();
 	});
 
 	io.emitter.on('wake', () => {
+		console.debug(TAG, 'wake');
 		RaspiLeds.setColor(colorForHotword);
 	});
 
 	io.emitter.on('stop', () => {
+		console.debug(TAG, 'stop');
 		RaspiLeds.off();
 	});
 };
