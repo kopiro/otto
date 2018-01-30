ab8195cddbaf574ae793b697d553887164593e87
kopiro <destefano.flavio@gmail.com>
2018-01-30 22:35:40 +0100

    Added feature

--- src/actions/maps/distance.js
+++ src/actions/maps/distance.js
@@ -8,17 +8,18 @@ module.exports = function({ sessionId, result }) {
 		let { parameters: p, fulfillment } = result;
 		
 		GoogleMaps.directions(p, (err, response) => {
-			if (err) reject(err);
+			if (err) return;
 
 			const distance = response.json.routes[0].legs[0].distance.text;
+
 			const duration = response.json.routes[0].legs[0].duration.value;
 			const duration_human = Moment.duration(duration * 1000).humanize();
 
 			resolve({
 				speech: fulfillment.speech
-						.replace('$_distance', distance)
-						.replace('$_duration', duration_human)
-			});
+					.replace('$_distance', distance)
+					.replace('$_duration', duration_human)
+			});		
 		});
 	});
 };
