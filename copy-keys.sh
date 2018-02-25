cffc4a716e0deb81696072d82c2b906bed18dacd
Flavio De Stefano <destefano.flavio@gmail.com>
2018-02-25 13:31:02 +0100

    Async copy keys

--- copy-keys.sh
+++ copy-keys.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -x
-scp -r keys root@kopiro.it:/home/otto
-scp -r keys root@otto-home.local:/root
-scp -r keys root@otto.local:/root
+scp -r keys root@kopiro.it:/home/otto &
+scp -r keys root@otto-home.local:/root &
+scp -r keys root@otto.local:/root &
