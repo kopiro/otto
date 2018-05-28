8225ccec30e08292830148b24e68a319b5a8420e
Flavio De Stefano <destefano.flavio@gmail.com>
2018-05-28 22:54:07 +0200

    Reverting to client credentials on spotify

--- src/lib/spotify.js
+++ src/lib/spotify.js
@@ -3,14 +3,19 @@ const TAG = 'Spotify';
 const _config = config.spotify;
 
 const SpotifyWebApi = require('spotify-web-api-node');
-const SpotifyWPAT = require('spotify-webplayer-accesstoken');
 
-const client = new SpotifyWebApi();
+const client = new SpotifyWebApi(_config);
+
+// const SpotifyWPAT = require('spotify-webplayer-accesstoken');
+// client.connect = async function() {
+// 	const access_token = await SpotifyWPAT.getAccessToken(_config.username, _config.password);
+// 	client.setAccessToken(access_token);
+// 	client.connected = true;
+// };
 
 client.connect = async function() {
-	const access_token = await SpotifyWPAT.getAccessToken(_config.username, _config.password);
-	client.setAccessToken(access_token);
-	client.connected = true;
+	const e = await client.clientCredentialsGrant();
+	client.setAccessToken(e.body.access_token);
 };
 
 module.exports = client;
