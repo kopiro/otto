9d0301929be57e8462ec2dee0d5b214827ade135
Flavio De Stefano <destefano.flavio@gmail.com>
2018-10-27 23:45:31 +0200

    Fix syntax herror

--- src/scheduler/good_night.js
+++ src/scheduler/good_night.js
@@ -1,7 +1,9 @@
 const TAG = 'Scheduler/GoodNight';
 
-exports.run = function({ session }) {
-	return IOManager.hanle({
+exports.run = function ({
+	session
+}) {
+	return IOManager.handle({
 		session: session,
 		params: {
 			event: 'good_night'
