8d8478c9c5fc41cdbd3ae0a4b27795a19e67c705
Flavio De Stefano <destefano.flavio@gmail.com>
2018-11-06 10:22:13 +0100

    Reformat config

--- test/rest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# curl "http://localhost:9000/io/rest?sessionId=test" -X POST --data "text=Ciao"
-
-# curl "http://localhost:9000/io/rest?sessionId=test" -X POST -F "audio=@hello.m4a"
-
-curl "http://localhost:9000/io/rest?sessionId=test&outputType=voice" -X POST --data "text=Papà"
