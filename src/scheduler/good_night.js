151daf1f82f2356548cb3f324537e066868dfde7
Flavio De Stefano <destefano.flavio@gmail.com>
2019-02-11 21:39:23 +0100

    Fix event good_night

--- src/scheduler/good_night.js
+++ src/scheduler/good_night.js
@@ -1,5 +1,5 @@
-const TAG = 'Scheduler/GoodMorning';
+const TAG = 'Scheduler/GoodNight';
 
 exports.run = function({ session }) {
-	return IOManager.outputByInputParams({ event: 'good_morning' }, session);
+	return IOManager.outputByInputParams({ event: 'good_night' }, session);
 };
