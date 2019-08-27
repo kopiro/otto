1bac1f0725bd93a214b0c1caffd2ac0135d984c4
Flavio De Stefano <destefano.flavio@gmail.com>
2019-08-27 23:16:16 +0200

    Use post instead

--- src/listeners/io_event.js
+++ src/listeners/io_event.js
@@ -3,23 +3,23 @@ const AI = require('../stdlib/ai');
 const IOManager = require('../stdlib/iomanager');
 
 const run = () => {
-  Server.routerListeners.get('/io_event', async (req, res) => {
-    if (!req.query.event) {
+  Server.routerListeners.post('/io_event', async (req, res) => {
+    if (!req.body.event) {
       return res.status(400).json({
         error: 'QUERY_EVENT_EMPTY'
       });
     }
-    if (!req.query.session) {
+    if (!req.body.session) {
       return res.status(400).json({
         error: 'QUERY_SESSION_EMPTY'
       });
     }
-    const session = await IOManager.getSession(req.query.session);
+    const session = await IOManager.getSession(req.body.session);
     if (!session) {
       return res.status(400).json({ error: 'SESSION_NOT_FOUND' });
     }
     const output = await AI.processInput({
-      params: { event: req.query.event },
+      params: { event: req.body.event },
       session
     });
     res.json({ status: output });
