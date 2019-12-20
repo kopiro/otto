edd7b89bc2feb345f4d053a7d0d58f858e9b06e4
Flavio De Stefano <destefano.flavio@gmail.com>
2019-12-20 12:26:13 +0100

    Fix raspi deps

--- client_deps/raspberry.sh
+++ client_deps/raspberry.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-apt-get update
+sudo apt-get update
 
-apt-get -y install sox
-apt-get -y install libsox-fmt-all
-apt-get -y install opus-tools
-apt-get -y install libmagic-dev libatlas-base-dev
+sudo apt-get -y install sox
+sudo apt-get -y install libsox-fmt-all
+sudo apt-get -y install opus-tools
+sudo apt-get -y install libmagic-dev libatlas-base-dev
 
-wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
-wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/jessie.list
-apt-get update
-apt-get -y install mopidy mopidy-spotify mpc
+sudo apt-get -y install libavahi-compat-libdnssd-dev
 
-apt-get -y install libavahi-compat-libdnssd-dev
-
-if which node > /dev/null
-then
-	echo "Node has already been installed"
-else
-	wget -O - https://raw.githubusercontent.com/audstanley/NodeJs-Raspberry-Pi/master/Install-Node.sh | bash
-fi
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.2/install.sh | bash
+source ~/.bash_profile
+nvm install 8
 
 npm -g install yarn
 
