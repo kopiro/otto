10632256d0fcd6e6b7557a7c22b544ff0dd27012
Flavio De Stefano <destefano.flavio@gmail.com>
2020-07-11 18:16:24 +0200

    Do not count in reverse

--- src/scheduler/countdown.ts
+++ src/scheduler/countdown.ts
@@ -7,8 +7,13 @@ export default class CountdownScheduler extends SchedulerProgramClass {
   async run() {
     const { date, name, eventName } = this.job.programArgs;
     const momentDate = Moment(date);
+    if (new Date() > momentDate.toDate()) {
+      return;
+    }
+
     const time = momentDate.fromNow();
     const timeNoSuffix = momentDate.fromNow(true);
+
     return AI.processInput(
       {
         event: {
