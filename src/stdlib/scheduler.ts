c478fd91c517cdbd479e27acc9b21a257d31c401
Flavio De Stefano <destefano.flavio@gmail.com>
2020-08-16 01:45:50 +0200

    Make query debuggable

--- src/stdlib/scheduler.ts
+++ src/stdlib/scheduler.ts
@@ -21,20 +21,21 @@ export class Scheduler {
 
   async getJobs(conditions = []): Promise<SchedulerModel[]> {
     const time = Moment().seconds(0); //Get current time but reset seconds to zero
+    const query = [
+      { yearly: time.format("DDD HH:mm:ss") },
+      { monthly: time.format("D HH:mm:ss") },
+      { weekly: time.format("d HH:mm:ss") },
+      { daily: time.format("HH:mm:ss") },
+      { hourly: time.format("mm:ss") },
+      { minutely: time.format("ss") },
+      { onDate: time.format(FORMAT) },
+      { onTick: true },
+      { dailyRandom: true },
+      ...conditions,
+    ];
     const jobs = await Data.Scheduler.find({
       managerUid: config().uid,
-      $or: [
-        { yearly: time.format("DDD HH:mm:ss") },
-        { monthly: time.format("D HH:mm:ss") },
-        { weekly: time.format("d HH:mm:ss") },
-        { daily: time.format("HH:mm:ss") },
-        { hourly: time.format("mm:ss") },
-        { minutely: time.format("ss") },
-        { onDate: time.format(FORMAT) },
-        { onTick: true },
-        { dailyRandom: true },
-        ...conditions,
-      ],
+      $or: query,
     });
     return jobs;
   }
