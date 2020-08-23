7aed810a3c5265c7425c562dddc81cf85d8c5517
Flavio De Stefano <destefano.flavio@gmail.com>
2020-08-23 20:19:02 +0200

    -Ex auto on raspistill

--- src/lib/raspi-camera.ts
+++ src/lib/raspi-camera.ts
@@ -5,15 +5,15 @@ import { getTmpFile } from "../helpers";
 export class RaspiCamera extends Camera {
   async takePhoto(): Promise<string> {
     const tmpFile = getTmpFile("jpg");
-    await Proc.spawn("raspistill", ["-o", tmpFile]);
+    await Proc.spawn("raspistill", ["-o", tmpFile, "-ex", "auto"]);
     return tmpFile;
   }
   async takeVideo(time = 5): Promise<string> {
     const tmpFileH264 = getTmpFile("h264");
     const tmpFileAudio = getTmpFile("wav");
     await Promise.all([
-      Proc.spawn("raspivid", ["-t", time * 1000, "-o", tmpFileH264]),
       Proc.spawn("arecord", ["-d", time, "-c", "2", "-f", "s16_LE", "-r", 8000, tmpFileAudio]),
+      Proc.spawn("raspivid", ["-t", time * 1000, "-o", tmpFileH264]),
     ]);
     const tmpFileVideo = getTmpFile("mp4");
     await Proc.spawn("ffmpeg", [
