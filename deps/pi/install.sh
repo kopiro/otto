e79d9ed1092a612fd1522af41e0398d7f5b7d0dc
Flavio De Stefano <destefano.flavio@gmail.com>
2022-05-23 13:33:49 +0200

    Polish pi/install

--- deps/pi/install.sh
+++ deps/pi/install.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-sudo apt update
-
-sudo apt -y install sox
-sudo apt -y install libsox-fmt-all
-sudo apt -y install opus-tools
-sudo apt -y install libmagic-dev libatlas-base-dev
-
-sudo apt -y install libavahi-compat-libdnssd-dev
-
-sudo apt -y install raspistill raspivid gpac
+sudo apt-get -y install \
+    sox \
+    libsox-fmt-all \
+    opus-tools \
+    raspistill \
+    raspivid \
+    gpac
 
 npm -g install yarn
-
 yarn install --production
