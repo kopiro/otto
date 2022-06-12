0aa6532443de5b16ebedb2fa54cd282fa9b353c8
Flavio De Stefano <destefano.flavio@gmail.com>
2022-06-12 19:56:16 +0200

    Fix null

--- src/stdlib/ai.ts
+++ src/stdlib/ai.ts
@@ -351,7 +351,7 @@ class AI {
   }
 
   private extractMessages(fulfillmentMessages: protos.google.cloud.dialogflow.v2.Intent.IMessage[], key: string) {
-    return fulfillmentMessages.find((m) => m?.payload?.fields?.[key] !== undefined)[key].stringValue;
+    return fulfillmentMessages.find((m) => m?.payload?.fields?.[key] !== undefined)?.[key]?.stringValue;
   }
 
   /**
