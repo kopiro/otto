1520277f084836b738e76e6bb9011a047e9552f5
Flavio De Stefano <destefano.flavio@gmail.com>
2023-06-06 14:38:05 +0200

    Fixing LTMR

--- src/stdlib/ai/long-term-memory-reducer.ts
+++ src/stdlib/ai/long-term-memory-reducer.ts
@@ -10,114 +10,111 @@ const console = new Signale({
   scope: TAG,
 });
 
+const MAX_CHARS = 300;
+
 /**
  * This module is responsible for reducing the long term memory of the AI.
  * It will parse all the "Interaction" records in the database every 24 hours and reduce it using AI algorithms, then save it bac to the table "LongTermMemory".
  */
 
-type GroupedInteractionsByDay = Record<number, IInteraction[]>;
-type GroupedInteractionsBySessionAndDay = Record<string, GroupedInteractionsByDay>;
+type GroupedInteractionsBySession = Record<string, IInteraction[]>;
+type GroupedInteractionsByDayThenSession = Record<number, GroupedInteractionsBySession>;
 
 export class LongTermMemoryReducer {
-  async getInteractionsGroupedBySessionAndDay(): Promise<GroupedInteractionsBySessionAndDay> {
+  async getInteractionsGroupedByDayThenSession(): Promise<GroupedInteractionsByDayThenSession> {
     const unreducedInteractions = await Interaction.find({
       reducedLongTermMemory: { $exists: false },
     }).sort({ createdAt: +1 });
 
-    const groupedInteractionsBySessionAndDay = unreducedInteractions.reduce((acc, interaction) => {
-      if (!acc[interaction.session.id]) {
-        acc[interaction.session.id] = [];
-      }
+    const groupedInteractionsByDayThenSession = unreducedInteractions.reduce((acc, interaction) => {
       const daySince1970 = Math.floor(interaction.createdAt.getTime() / (1000 * 60 * 60 * 24));
-      acc[interaction.session.id] = acc[interaction.session.id] || {};
-      acc[interaction.session.id][daySince1970] = acc[interaction.session.id][daySince1970] || [];
-      acc[interaction.session.id][daySince1970].push(interaction);
+      acc[daySince1970] = acc[daySince1970] || {};
+      acc[daySince1970][interaction.session.id] = acc[daySince1970][interaction.session.id] || [];
+      acc[daySince1970][interaction.session.id].push(interaction);
       return acc;
-    }, {} as GroupedInteractionsBySessionAndDay);
-    return groupedInteractionsBySessionAndDay;
+    }, {} as GroupedInteractionsByDayThenSession);
+
+    return groupedInteractionsByDayThenSession;
   }
 
-  async saveLongTermMemory(session: ISession, date: Date, text: string): Promise<ILongTermMemory> {
+  async saveLongTermMemory(forDate: Date, text: string): Promise<ILongTermMemory> {
     const longTermMemory = new LongTermMemory({
-      session,
       text,
       createdAt: new Date(),
       type: "daily",
-      forDate: date,
+      forDate,
     });
     await longTermMemory.save();
     return longTermMemory;
   }
 
-  async markInteractionsAsReduced(longTermMemory: ILongTermMemory, interactions: IInteraction[]): Promise<void> {
-    const interactionIds = interactions.map((interaction) => interaction.id);
+  async markInteractionsAsReduced(longTermMemory: ILongTermMemory, interactionsIds: string[]): Promise<void> {
     await Interaction.updateMany(
-      { _id: { $in: interactionIds } },
+      { _id: { $in: interactionsIds } },
       { $set: { reducedLongTermMemory: longTermMemory.id } },
       { multi: true },
     );
   }
 
-  async reduceInteractions(session: ISession, interactions: IInteraction[]): Promise<string> {
+  async reduceInteractionsOfTheDay(
+    forDate: Date,
+    groupedInteractionsBySession: GroupedInteractionsBySession,
+  ): Promise<string> {
+    const { aiName } = config();
     const interactionsText = [];
 
-    const sessionName = getSessionName(session);
-    const aiName = config().aiName;
-
-    interactions.forEach((interaction) => {
-      const time =
-        interaction.createdAt.getHours() +
-        ":" +
-        interaction.createdAt.getMinutes() +
-        ":" +
-        interaction.createdAt.getSeconds();
-      if (interaction.fulfillment.text) {
-        interactionsText.push(`${aiName} (${time}): ${interaction.fulfillment.text}`);
+    for (const [sessionId, interactions] of Object.entries(groupedInteractionsBySession)) {
+      const sessionName = getSessionName(interactions[0].session);
+      interactionsText.push(`Conversation with ${sessionName}:`);
+      for (const interaction of interactions) {
+        const time = interaction.createdAt.toLocaleTimeString();
+        if (interaction.fulfillment.text) {
+          interactionsText.push(`${aiName} (${time}): ${interaction.fulfillment.text}`);
+        }
+        if (interaction.input.text) {
+          interactionsText.push(`USER (${time}): ${interaction.input.text}`);
+        }
       }
-      if (interaction.input.text) {
-        interactionsText.push(`${sessionName} (${time}): ${interaction.input.text}`);
-      }
-    });
+      interactionsText.push("\n");
+    }
 
-    const prompt =
-      `I have the following interactions between ${aiName} and a ${sessionName}, please reduce them to a single sentence below 200 characters.\n\n` +
+    const reducerPrompt =
+      `I have the following interactions between ${aiName} and users, happening at ${forDate.toDateString()}, please reduce them to a single sentence. Keep the output length below ${MAX_CHARS} characters, and compress it to make it as short as possible. In case of errors, return strictly the following string "ERROR".\n\n` +
       interactionsText.join("\n");
 
-    const reducedMemory = await openai().textRequest(prompt, session, "system", "only_memories");
-    console.debug("reducedMemory :>> ", reducedMemory);
+    const reducedMemory = await openai().textRequest(reducerPrompt, null, "system", "none");
+    if (reducedMemory.trim() === "ERROR") {
+      return "";
+    }
 
     return reducedMemory;
   }
 
   async reduce(): Promise<void> {
-    const interactions = await this.getInteractionsGroupedBySessionAndDay();
-    console.info("Found " + Object.keys(interactions).length + " total interactions to reduce");
+    const interactions = await this.getInteractionsGroupedByDayThenSession();
+    console.info("Found " + Object.keys(interactions).length + " total days to reduce");
 
-    for (const sessionId in interactions) {
-      console.info(`Reducing interactions for session ${sessionId}`);
+    for (const day in interactions) {
+      try {
+        const forDate = new Date(Number(day) * 1000 * 60 * 60 * 24);
+        console.info(`Reducing interactions for Date: ${forDate.toDateString()}`);
 
-      const session = await Session.findById(sessionId);
-      if (!session) {
-        console.error(`\tSession ${sessionId} not found`);
-        continue;
-      }
+        // Extract all interactions IDs
+        const allInteractionIds = Object.values(interactions[day]).reduce((acc, interactions) => {
+          acc.push(...interactions.reduce((acc, interaction) => [...acc, interaction.id], []));
+          return acc;
+        }, [] as string[]);
 
-      for (const day in interactions[sessionId]) {
-        try {
-          const dateFromDay = new Date(Number(day) * 1000 * 60 * 60 * 24);
-          const _interactions = interactions[sessionId][day];
-          console.info(`\tFound ${_interactions.length} interactions for day ${dateFromDay.toISOString()}`);
+        console.info(`Found ${allInteractionIds.length} interactions to reduce`);
+        const reducedInteractionText = await this.reduceInteractionsOfTheDay(forDate, interactions[day]);
 
-          const reducedInteractionText = await this.reduceInteractions(session, _interactions);
+        const longTermMemory = await this.saveLongTermMemory(forDate, reducedInteractionText);
+        console.info(`Saved long term memory: ${longTermMemory.id}`);
 
-          const longTermMemory = await this.saveLongTermMemory(session, dateFromDay, reducedInteractionText);
-          console.info(`\tSaved long term memory: ${longTermMemory.id}`);
-
-          await this.markInteractionsAsReduced(longTermMemory, _interactions);
-          console.info(`\tMarked ${_interactions.length} interactions as reduced`);
-        } catch (err) {
-          console.error(`\tError reducing interactions`, err);
-        }
+        await this.markInteractionsAsReduced(longTermMemory, allInteractionIds);
+        console.info(`Marked ${allInteractionIds.length} interactions as reduced`);
+      } catch (err) {
+        console.error(`Error reducing interactions`, err);
       }
     }
   }
