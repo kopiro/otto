9e18d00e7080d19a21d968206553044cbf7cfead
Flavio De Stefano <destefano.flavio@gmail.com>
2023-08-09 09:52:03 +0200

    Add period when chr13

--- src/lib/text-to-speech/google-text-to-speech.ts
+++ src/lib/text-to-speech/google-text-to-speech.ts
@@ -30,10 +30,12 @@ export class GoogleTextToSpeech implements ITextToSpeech {
 
   private cleanText(text: string) {
     // Removi all emojies
-    return text.replace(
-      /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
-      "",
-    );
+    return text
+      .replace(
+        /([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g,
+        "",
+      )
+      .replace("\n", ".");
   }
 
   private async getVoice(language: Language, gender: Gender): Promise<google.cloud.texttospeech.v1beta1.IVoice> {
