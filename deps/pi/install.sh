016af60ebd240aa47c2f94c076513678d34f6f17
Flavio De Stefano <destefano.flavio@gmail.com>
2025-02-16 18:30:45 +0100

    Fix systemd

--- deps/pi/install.sh
+++ deps/pi/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # git clone https://github.com/kopiro/otto.git ai
-# git pull
 # ./deps/pi/install.sh
 
 # Chck if we running as "root"
@@ -13,6 +12,7 @@ fi
 NODE_MAJOR=22
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /usr/share/keyrings/nodesource.gpg
 echo "deb [signed-by=/usr/share/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+apt update
 
 sudo apt-get -y install \
     sox \
@@ -20,6 +20,11 @@ sudo apt-get -y install \
     opus-tools \
     nodejs
 
-runuser -l otto -c 'cd ~/ai; mkdir -p ~/.config/systemd/user; cp ./deps/pi/otto.service ~/.config/systemd/user/otto.service; systemctl --user daemon-reload'
+corepack enable
 
 runuser -l otto -c 'cd ~/ai && pnpm install'
+
+cp ./deps/pi/otto.service /etc/systemd/system/otto.service
+systemctl daemon-reload
+
+systemctl enable otto
